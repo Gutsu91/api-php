2ebee7958999a812d4c81a5c909ceaed5efc191d
Gutsu <titeca.f@gmail.com>
2022-07-06 16:45:26 +0200

    ajout du delete produit, delete categorie non fonctionnel

--- categorie.php
+++ categorie.php
@@ -3,6 +3,7 @@ require_once 'config.php';
 require_once 'headers.php';
 //require_once 'verif-token.php';
 
+/* Gestion du GET */
 if($_SERVER['REQUEST_METHOD'] == 'GET') :
   if(isset($_GET['id_categorie'])):
     $req_cat = sprintf("SELECT * FROM categorie LEFT JOIN product ON categorie.id_categorie = product.id_categorie_prod WHERE id_categorie = %d",
@@ -22,6 +23,26 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') :
   endwhile;
 endif;
 
-echo json_encode($categorie);
+/* Gestion du DELETE */
+if($_SERVER['REQUEST_METHOD'] == "DELETE") :
+  if(isset($_GET['id_categorie'])) : // on fait une requête sur la table produit pour voir s'il a des produits qui ont cette catégorie
+    $req_cat_empty = sprintf("SELECT * FROM product WHERE id_categorie_prod=%d",
+    $_GET['id_categorie']);
+    $categorie = $connect->query($req_cat_empty);
+    echo $connect->error;
+    $nbResult = $categorie->num_rows;
+    if($nbResult == 0 ) :
+      echo 'foo';
+    else:
+      $categorie['response'] = "Vous ne pouvez pas supprimer cette catégorie car elle contient encore des produits";
+      $categoriet['code'] = "NOK";
+    endif;
+  else:
+    $categorie['response'] = "Vous n'avez pas défini l'id de la catégorie à supprimer";
+    $categorie['code'] = "NOK";
+  endif;
+endif;
+/* post mortem : le delete ne fonctionne pas, j'ai du foirer dans mes checks */
 
+echo json_encode($categorie); // doit être la dernière ligne sinon il ne retournera pas de réponse
 ?>
--- product.php
+++ product.php
@@ -3,6 +3,7 @@ require_once 'config.php';
 require_once 'headers.php';
 //require_once 'verif-token.php';
 
+/* Gestion du GET */
 if($_SERVER['REQUEST_METHOD'] == 'GET') :
   if(isset($_GET['id_produit'])):
     $req_product = sprintf("SELECT * FROM product LEFT JOIN categorie ON product.id_categorie_prod = categorie.id_categorie WHERE id_produit=%d",
@@ -22,5 +23,19 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') :
   endwhile;
 endif;
 
-echo json_encode($product); // doit être la dernière ligne sinon il ne te retournera pas de réponse
-?>
+/* Gestion du DELETE */
+if($_SERVER['REQUEST_METHOD'] == "DELETE") : 
+  if(isset($_GET['id_produit'])) :
+    $req_product = sprintf("DELETE FROM product WHERE id_produit=%d",
+    $_GET['id_produit']);
+    $connect->query($req_product);
+    echo $connect->error;
+    $product['response'] = "Suppression du produit avec l'id " . $_GET['id_produit'];
+    $product['code'] = "OK";
+  else:
+    $product['response'] = "vous n'avez pas défini l'id du produit à supprimer";
+    $product['code'] = "NOK";
+  endif;
+endif;
+
+echo json_encode($product); // doit être la dernière ligne sinon il ne te retournera pas de réponse?>
